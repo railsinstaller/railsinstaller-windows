8539fdf3e01ad4cf8ec02613b092578cd246ba7b
Luis Lavena <luislavena@gmail.com>
2011-01-11 19:25:49 -0300

    Add git to setup_environment

--- resources/railsinstaller/setup_environment.bat
+++ resources/railsinstaller/setup_environment.bat
@@ -4,11 +4,17 @@ PUSHD %~dp0.
 SET RUBY_BIN=%CD%
 POPD
 
-REM Add RUBY_BIN to the PATH
-REM RUBY_BIN takes higher priority to avoid other tools
-REM conflict with our own (mainly the DevKit)
-SET PATH=%RUBY_BIN%;%PATH%
-SET RUBY_BIN=
+REM Now determine Root (.. from RUBY_BIN)
+PUSHD %RUBY_BIN%\..
+SET ROOT_DIR=%CD%
+POPD
 
-REM Display Ruby version
+REM Add RUBY_BIN to the PATH, DevKit and then Git
+REM RUBY_BIN takes higher priority to avoid other tools conflict
+SET PATH=%RUBY_BIN%;%ROOT_DIR%\DevKit;%ROOT_DIR%\Git\cmd;%PATH%
+SET RUBY_BIN=
+SET ROOT_DIR=
+
+REM Display Ruby version and Git versions
 ruby.exe -v
+git --version
