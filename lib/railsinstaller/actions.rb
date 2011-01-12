7b94524b81fa2a848a0ad6a96ff52c601018babb
Wayne E. Seguin <wayneeseguin@gmail.com>
2011-01-12 16:59:11 -0500

    Adjusted for .txt appending.

--- lib/railsinstaller/actions.rb
+++ lib/railsinstaller/actions.rb
@@ -32,7 +32,7 @@ module RailsInstaller
   # self contained installer using Inno Setup scripting.
   #
   def self.package!
-    version = File.read(File.join(RailsInstaller::Root, "VERSION")).chomp
+    version = File.read(File.join(RailsInstaller::Root, "VERSION.txt")).chomp
 
     printf "Packaging... this *will* take a while...\n"
 
