3e9459f432a87057e24f24d7770396bdd3bf0b89
Wayne E. Seguin <wayneeseguin@gmail.com>
2011-02-01 17:07:16 -0500

    Added creation of HOME\Sites directory to setup_environment.bat

--- resources/railsinstaller/setup_environment.bat
+++ resources/railsinstaller/setup_environment.bat
@@ -1,25 +1,50 @@
+REM
+REM Environment setup file for RailsInstaller.
+REM
+
 @ECHO OFF
-REM Determine where is RUBY_DIR (which is where this script is)
+REM
+REM First we Determine where is RUBY_DIR
+REM (which is where this script is)
+REM
 PUSHD %~dp0.
 SET RUBY_DIR=%CD%
 POPD
 
-REM Determine RailsInstaller Root (parent directory of Ruby)
+REM
+REM Now we Determine the RailsInstaller Root directory
+REM (parent directory of Ruby)
+REM
 PUSHD %RUBY_DIR%\..
 SET ROOT_DIR=%CD%
 POPD
 
+REM
 REM Add RUBY_DIR\bin to the PATH, DevKit\bin and then Git\cmd
 REM RUBY_DIR\bin takes higher priority to avoid other tools conflict
+REM
 SET PATH=%RUBY_DIR%\bin;%RUBY_DIR%lib\ruby\gems\1.8\bin;%ROOT_DIR%\DevKit\bin;%ROOT_DIR%\Git\cmd;%PATH%
 SET RUBY_DIR=
 SET ROOT_DIR=
 
+REM
+REM Create the HOME\Sites directory.
+REM
+IF NOT EXIST %HOMEDRIVE%%HOMEPATH%\Sites. (md %HOMEDRIVE%%HOMEPATH%\Sites.)
+
+REM
+REM Set the HOME environment variables for Ruby & Gems to use
+REM with ENV["HOME"]
+REM
 SET HOME=%HOMEDRIVE%%HOMEPATH%
 
-REM Display Ruby and Git version
+REM Display Git, Ruby and Rails versions
 git --version
-ruby.exe -v
+
+ruby -v
+
 rails -v
 
 REM NOTE we start out in the Sites directory as that is the working dir set.
+REM cd %HOMEDRIVE%%HOMEPATH%\Sites
+
