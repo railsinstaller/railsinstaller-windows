dc9ef0b70141d6504255bb30cbae974a7503c6b3
Wayne E. Seguin <wayneeseguin@gmail.com>
2011-04-12 10:18:06 -0400

    Properly load pubkey.

--- resources/scripts/publickey.bat
+++ resources/scripts/publickey.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Projects/railsinstaller/stage/Ruby1.8.7/bin/sshkey" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ruby.exe" "require 'rbconfig' ; load %Q{#{RbConfig::Config['bindir']}/pubkey}" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ruby.exe" "%~dpn0" %*
