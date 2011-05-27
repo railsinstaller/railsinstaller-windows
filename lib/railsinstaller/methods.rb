41d1f4a14c774615f517bb5306014d46ee288fd5
Wayne E. Seguin <wayneeseguin@gmail.com>
2011-05-27 16:09:46 -0400

    Added quotes and debug.

--- lib/railsinstaller/methods.rb
+++ lib/railsinstaller/methods.rb
@@ -324,11 +324,12 @@ module RailsInstaller
   def self.gem_install(ruby_path,gem,options={})
     printf " => Staging gem #{gem}\n" if $Flags[:verbose]
     %w(GEM_HOME GEM_PATH).each { |variable| ENV.delete(variable)}
-    line = %Q(#{File.join(ruby_path, "bin", "gem")} install #{gem})
-    line += %Q( -v#{options[:version]} ) if options[:version]
+    line = %Q(#{File.join(ruby_path, "bin", "gem")} install #{gem} )
+    line += %Q( -v'#{options[:version]}' ) if options[:version]
     line += %Q( --env-shebang --no-rdoc --no-ri )
     line += options[:args] if options[:args]
 
+    printf("DEBUG> %s\n", line) if $Flags[:verbose]
     sh line
   end
 
