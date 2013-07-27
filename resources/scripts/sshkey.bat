9ddc5953abb13bec8b637c38254c8114a804fb6f
Evan Machnic <emachnic@engineyard.com>
2013-07-26 21:00:13 -0400

    Fixed sshkey.bat and bumped Ruby patch version

--- resources/scripts/sshkey.bat
+++ resources/scripts/sshkey.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Projects/railsinstaller/stage/Ruby1.9.2/bin/sshkey" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ruby.exe" "C:/Projects/railsinstaller/stage/Ruby1.9.3/bin/sshkey" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ruby.exe" "%~dpn0" %*
