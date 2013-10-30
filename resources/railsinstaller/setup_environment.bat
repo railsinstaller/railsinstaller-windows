61783f7d982fb26ac7ab92606091c68d92dab9ce
Kelvin Raffael Stinghen <kelvin.stinghen@gmail.com>
2013-10-29 22:26:58 -0200

    Workspace env variable

--- resources/railsinstaller/setup_environment.bat
+++ resources/railsinstaller/setup_environment.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM
-REM Environment setup file for RailsInstaller.
+REM "Environment setup file for RailsInstaller."
 REM
 
 REM
@@ -19,8 +19,8 @@ SET ROOT_DIR=%CD%
 POPD
 
 REM
-REM Add RUBY_DIR\bin to the PATH, DevKit\bin and then Git\cmd
-REM RUBY_DIR\bin takes higher priority to avoid other tools conflict
+REM "Add RUBY_DIR\bin to the PATH, DevKit\bin and then Git\cmd"
+REM "RUBY_DIR\bin takes higher priority to avoid other tools conflict"
 REM
 SET PATH=%RUBY_DIR%\bin;%RUBY_DIR%\lib\ruby\gems\1.9.1\bin;%ROOT_DIR%\DevKit\bin;%PATH%
 IF EXIST %ROOT_DIR%\Git\cmd SET PATH=%ROOT_DIR%\Git\cmd;%PATH%
@@ -38,7 +38,7 @@ REM
 SET HOME=%HOMEDRIVE%%HOMEPATH%
 
 SET RailsInstallerPath=%1
-REM Check configurations for Git and SSH
+REM "Check configurations for Git and SSH"
 IF EXIST %RailsInstallerPath% (
   ruby %RailsInstallerPath%\scripts\config_check.rb
 ) ELSE (
@@ -47,3 +47,16 @@ IF EXIST %RailsInstallerPath% (
 
 REM NOTE that we start out in the Sites directory as the current working dir
 IF EXIST %HOMEDRIVE%\Sites CD %HOMEDRIVE%\Sites
+REM
+REM "Set the RAILS_WORKSPACE only if the user doesn't already did it"
+REM
+IF [%RAILS_WORKSPACE%] == [] (
+  SET RAILS_WORKSPACE=%HOMEDRIVE%\Sites
+)
+
+REM
+REM "Create the RAILS_WORKSPACE directory and cd it."
+REM
+IF NOT EXIST %RAILS_WORKSPACE% (md %RAILS_WORKSPACE%.)
+cd %RAILS_WORKSPACE%
+
